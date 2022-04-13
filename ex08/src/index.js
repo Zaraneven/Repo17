6e0557a2066b74a6ffba428136bb89a7d76d6bf8
neven kusturica <neven.kusturica@gmail.com>
2022-04-14 01:08:38 +0200

    Added ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+var users = [
+      {firstName: "John", lastName: "Doe", age: 24, gender: "male"},
+      {firstName: "Jane Doe", lastName: "Doe", age: "5", gender: "female"},
+      {firstName: "Jim", lastName: "Carrey", age: "54", gender: "male"},
+      {firstName: "Kate", lastName: "Winslet", age: "40", gender: "female"}
+];
+function getUser() {
+    var output = "";
+    for (output = 0; output < users.length; output++) {
+        console.log(getUser());
+        
+    
+        return output;
+        
+    }
+}
+
