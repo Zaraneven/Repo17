ac55924d21e2f77126bbd79da13e1e573b836ad6
neven kusturica <neven.kusturica@gmail.com>
2022-04-13 21:38:48 +0200

    Added ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,8 @@
+const workDays = ['MON', 'TUE', 'WED', 'THU', 'FRI'];
+
+function myWeek(days) {
+    let week = [...days, 'SAT', 'SUN'];
+    return week;
+}
+console.log(myWeek(workDays));
+module.exports = myWeek;
